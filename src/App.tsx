da483b3b7f21ac32434277f81a565a78c5232cf7
Beata <mlicka.beata@gmail.com>
2022-06-27 23:33:00 +0200

    fetching data and reducer's saving function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 
-function App() {
+const ACTIONS = {
+  SAVE_DATA: 'SAVE_DATA',
+}
+
+const reducer = (state: any, action: any) => {
+  switch(action.type) {
+    case ACTIONS.SAVE_DATA:
+      console.log('SAVE DATA - ', action)
+      return action.payload.results
+    default:
+      return state
+  }
+}
+
+const App = () => {
+  const [data, dispatch] = useReducer(reducer, {})
+
+  const fetchData = async () => {
+    const response = await fetch('https://swapi.dev/api/people/')
+    const responseJSON = await response.json()
+    dispatch({ type: ACTIONS.SAVE_DATA, payload: responseJSON})
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
     </div>
