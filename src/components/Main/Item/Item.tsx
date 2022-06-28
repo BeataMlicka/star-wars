a034666d9f58bb6991f72310c13d567e894bf5f6
Beata <mlicka.beata@gmail.com>
2022-06-28 12:29:48 +0200

    grid items style added

---
+++ src/components/Main/Item/Item.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Item: React.FC = () => {
+
+  return (
+    <div className='item' >
+      xyz
+    </div>
+  )
+}
+
+export default Item
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 import Character from '../../types/Character'
+import Item from './Item'
 
 type MainTypes = {
   list: Array<Character>,
 }
 
 const Main: React.FC<MainTypes> = ({ list }) => {
-
-  console.log('LIST - ', list)
   if(!list.length) return null
   return (
     <div className='main'>
-      Hello World
+      {
+        list.map((item) => <Item />)
+      }
     </div>
   )
 }
