c7d404297c86d407ccff71323483aee3f957385f
Beata <mlicka.beata@gmail.com>
2022-06-28 21:23:04 +0200

    onFocus and onBlur event added to toggle

--- src/components/Footer/Checkbox/Checkbox.tsx
+++ src/components/Footer/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 type CheckboxTypes = {
   onChange: any,
@@ -6,15 +6,23 @@ type CheckboxTypes = {
 
 const Checkbox: React.FC<CheckboxTypes> = ({ onChange }) => {
   const [checkboxState, setChecboxState] = useState(false)
+  const [checkboxFocus, setChecboxFocus] = useState(false)
 
   const handleCheckboxChange = (event: any) => {
     setChecboxState(event.target.checked)
     onChange(event)
   }
 
+  console.log('CH ', checkboxFocus)
+
   return (
-    <div className='checkbox'>
-      <input type='checkbox' onChange={handleCheckboxChange} />
+    <div className={`checkbox ${checkboxFocus ? 'checkbox--focus' : ''}`}>
+      <input
+        type='checkbox'
+        onChange={handleCheckboxChange}
+        onFocus={() => setChecboxFocus(true)}
+        onBlur={() => setChecboxFocus(false)}
+      />
       <div className={`checkbox__button ${checkboxState ? 'checkbox__button--checked' : ''}`} />
     </div>
   )
